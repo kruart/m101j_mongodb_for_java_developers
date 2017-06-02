dde9efc01e94dfcb290e28a8f89d81739af0870c
weoz <weoz@ukr.net>
2017-06-02 13:10:33 +0300

    1.1 Intro to the Spark Web Application Framework

---
+++ src/main/java/com/mongodb/HelloWorldSparkStyle.java
@@ -0,0 +1,13 @@
+package com.mongodb;
+
+import spark.Spark;
+
+/**
+ * Created by kruart on 02.06.2017.
+ */
+public class HelloWorldSparkStyle {
+
+    public static void main(String[] args) {
+        Spark.get("/hello", (request, response) -> "Hello World From Spark"); //default port: 4567 => localhost:4567/hello
+    }
+}
