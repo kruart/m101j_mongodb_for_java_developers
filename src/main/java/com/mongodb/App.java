5211b732b22307fb8770533f546d1c8300e32e51
weoz <weoz@ukr.net>
2017-06-02 11:53:23 +0300

    Init project

---
+++ src/main/java/com/mongodb/App.java
@@ -0,0 +1,11 @@
+package com.mongodb;
+
+/**
+ * Created by kruart on 02.06.2017.
+ */
+public class App {
+
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+    }
+}
