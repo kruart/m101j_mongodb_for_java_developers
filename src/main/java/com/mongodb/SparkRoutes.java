04f721cfd9ed783752ce281e581b5fec5f4f0192
weoz <weoz@ukr.net>
2017-06-03 12:16:55 +0300

    1.4 Spark: Handling GET requests

---
+++ src/main/java/com/mongodb/SparkRoutes.java
@@ -0,0 +1,15 @@
+package com.mongodb;
+
+import spark.Spark;
+
+/**
+ * Created by kruart on 02.06.2017.
+ */
+public class SparkRoutes {
+
+    public static void main(String[] args) {
+        Spark.get("/", (request, response) -> "Hello World\n");
+        Spark.get("/test", (request, response) -> "This is a test page\n");
+        Spark.get("/echo/:thing", (request, response) -> request.params(":thing")); //:thing - placeholder, there can be everything you want
+    }
+}
