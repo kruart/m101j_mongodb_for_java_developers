fb7fee75ccadc428247ef56214da2de7e5d6b710
weoz <weoz@ukr.net>
2017-06-10 11:12:14 +0300

    2.2 connect to db and get collection

---
+++ src/main/java/com/mongodb/App.java
@@ -0,0 +1,20 @@
+package com.mongodb;
+
+import com.mongodb.client.MongoCollection;
+import com.mongodb.client.MongoDatabase;
+import org.bson.Document;
+
+/**
+ * Created by kruart on 09.06.2017.
+ */
+public class App {
+
+    public static void main(String[] args) {
+        MongoClientOptions options = MongoClientOptions.builder().connectionsPerHost(100).build();
+        MongoClient client = new MongoClient(new ServerAddress("localhost", 27017), options);
+
+        MongoDatabase db = client.getDatabase("video");
+        MongoCollection<Document> reviews = db.getCollection("reviews");
+        System.out.println(reviews.count());
+    }
+}
