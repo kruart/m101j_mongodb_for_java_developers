165c2fc58370a829c891a7da35c6fbceb1f7b051
weoz <weoz@ukr.net>
2017-06-10 11:45:18 +0300

    2.3 Java driver: Document representation

---
+++ src/main/java/com/mongodb/DocumentTest.java
@@ -0,0 +1,29 @@
+package com.mongodb;
+
+import org.bson.Document;
+import org.bson.types.ObjectId;
+
+import java.util.Arrays;
+import java.util.Date;
+
+/**
+ * Created by kruart on 10.06.2017.
+ */
+public class DocumentTest {
+
+    public static void main(String[] args) {
+        Document document = new Document()
+                .append("str", "MongoDB, Hello")
+                .append("int", 42)
+                .append("l", 1L)
+                .append("double", 20.30)
+                .append("b", false)
+                .append("date", new Date())
+                .append("objectID", new ObjectId())
+                .append("null", null)
+                .append("embeddedDoc", new Document("x", 0))
+                .append("list", Arrays.asList(1, 2, 3));
+
+        Helpers.printJson(document);
+    }
+}
---
+++ src/main/java/com/mongodb/Helpers.java
@@ -0,0 +1,24 @@
+package com.mongodb;
+
+import org.bson.Document;
+import org.bson.codecs.DocumentCodec;
+import org.bson.codecs.EncoderContext;
+import org.bson.json.JsonMode;
+import org.bson.json.JsonWriter;
+import org.bson.json.JsonWriterSettings;
+
+import java.io.StringWriter;
+
+/**
+ * Created by kruart on 10.06.2017.
+ */
+public class Helpers {
+    public static void printJson(Document document) {
+        JsonWriter jsonWriter = new JsonWriter(new StringWriter(), new JsonWriterSettings(JsonMode.SHELL, true)); //true - use formatting json
+        new DocumentCodec().encode(jsonWriter, document, EncoderContext.builder()
+                                                                       .isEncodingCollectibleDocument(true)
+                                                                       .build());
+        System.out.println(jsonWriter.getWriter());
+        System.out.flush();
+    }
+}
