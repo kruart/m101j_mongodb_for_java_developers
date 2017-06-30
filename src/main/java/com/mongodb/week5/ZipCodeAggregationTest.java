7618fcebdf1ca3d3e45f77fac1949eccac1a1e65
weoz <weoz@ukr.net>
2017-06-30 11:18:12 +0300

    5.3 json parse

--- src/main/java/com/mongodb/week5/ZipCodeAggregationTest.java
+++ src/main/java/com/mongodb/week5/ZipCodeAggregationTest.java
@@ -3,11 +3,7 @@ package com.mongodb.week5;
 import com.mongodb.MongoClient;
 import com.mongodb.client.MongoCollection;
 import com.mongodb.client.MongoDatabase;
-import com.mongodb.client.model.Accumulators;
-import com.mongodb.client.model.Aggregates;
-import com.mongodb.client.model.Filters;
 import org.bson.Document;
-import org.bson.conversions.Bson;
 
 import java.util.ArrayList;
 import java.util.Arrays;
@@ -23,13 +19,20 @@ public class ZipCodeAggregationTest {
         MongoDatabase db = client.getDatabase("course");
         MongoCollection<Document> zipcode = db.getCollection("zipcodes");
 
+//        1. verbosity way
 //        List<Document> pipeline = Arrays.asList(
 //                new Document("$group", new Document("_id", "$state")
 //                        .append("totalPop", new Document("$sum", "$pop"))),
 //                new Document("$match", new Document("totalPop", new Document("$gte", 10000000))));
 
-        List<Bson> pipeline = Arrays.asList(Aggregates.group("$state", Accumulators.sum("totalPop", "$pop")),
-                Aggregates.match(Filters.gte("totalPop", 10000000)));
+//        2. use specialized mongodb builders
+//        List<Bson> pipeline = Arrays.asList(Aggregates.group("$state", Accumulators.sum("totalPop", "$pop")),
+//                Aggregates.match(Filters.gte("totalPop", 10000000)));
+
+//        3. parse json
+        List<Document> pipeline = Arrays.asList(
+                Document.parse("{$group: {_id: '$state', totalPop: {$sum: '$pop'}}}"),
+                Document.parse("{$match: {totalPop: {$gte: 10000000 } } }"));
 
         ArrayList<Document> results = zipcode.aggregate(pipeline).into(new ArrayList<>());
 
